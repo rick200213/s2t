ba8b712e923e931f5a150b76c44f257ec39e7cc9
Aritra Bhattacharyya <rick200213@gmail.com>
2023-05-04 08:41:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
-import gradio as gr 
+import gradio as gr
 import os
-import sys
 import subprocess
-#from moviepy.editor import VideoFileClip
-
+import tempfile
 import whisper
 from whisper.utils import write_vtt
 
@@ -18,26 +16,31 @@ def video2mp3(video_file, output_ext="mp3"):
 
 
 def translate(input_video):
+    with tempfile.TemporaryDirectory() as tmpdir:
+        # Convert input video to mp3
+        audio_file = os.path.join(tmpdir, "audio.mp3")
+        subprocess.call(["ffmpeg", "-y", "-i", input_video, audio_file], 
+                        stdout=subprocess.DEVNULL,
+                        stderr=subprocess.STDOUT)
 
-    audio_file = video2mp3(input_video)
-    
-    options = dict(beam_size=5, best_of=5, fp16 = False)
-    translate_options = dict(task="translate", **options)
-    result = model.transcribe(audio_file,**translate_options)
+        # Transcribe audio using Whisper
+        options = dict(beam_size=5, best_of=5, fp16=False)
+        translate_options = dict(task="translate", **options)
+        result = model.transcribe(audio_file, **translate_options)
 
-    output_dir = '/content/'
-    audio_path = audio_file.split(".")[0]
+        # Write transcribed text to VTT file
+        subtitle = os.path.join(tmpdir, "subtitle.vtt")
+        with open(subtitle, "w") as vtt:
+            write_vtt(result["segments"], file=vtt)
 
-    with open(os.path.join(output_dir, audio_path + ".vtt"), "w") as vtt:
-      write_vtt(result["segments"], file=vtt)
+        # Add subtitles to input video
+        output_video = os.path.join(tmpdir, "output.mp4")
+        subprocess.call(["ffmpeg", "-y", "-i", input_video, "-vf", f"subtitles={subtitle}", output_video], 
+                        stdout=subprocess.DEVNULL,
+                        stderr=subprocess.STDOUT)
 
-    subtitle = audio_path + ".vtt"
-    output_video = audio_path + "_subtitled.mp4"
-
-    os.system(f"ffmpeg -i {input_video} -vf subtitles={subtitle} {output_video}")
-
-    return output_video
-
+        # Return path to the subtitled video
+        return output_video
 
 
 title = "Add Text/Caption to your YouTube Shorts - MultiLingual"
@@ -48,31 +51,19 @@ with block:
 
     with gr.Group():
         with gr.Box(): 
-
-           
-
             with gr.Row().style():
-               
                 inp_video = gr.Video(
                     label="Input Video",
                     type="filepath",
-                    mirror_webcam = False
+                    mirror_webcam=False
                 )
                 op_video = gr.Video()
             btn = gr.Button("Generate Subtitle Video")
-        
-        
-        
 
-
-        
         btn.click(translate, inputs=[inp_video], outputs=[op_video])
- 
+
         gr.HTML('''
-        
-                    Model by OpenAI - Gradio App by rick200213
-                    
-        
+            Model by OpenAI - Gradio App by rick200213
         ''')
 
-block.launch(debug = True)
+block.launch()
